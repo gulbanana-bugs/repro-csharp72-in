c63f72674e5257ee61ffec73205e171299ccc04b
Thomas Castiglione <thomas@agcom.com.au>
2017-12-08 20:37:49 +0800

    create repository

---
+++ IssueRepro-broken/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IssueReproRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string testMessage = "Line1|Field1|Field2\rLine2|Field3|Field4\r\r";
+
+            var message = ParseMessage(testMessage);
+            var line2 = message.FirstOrDefault(s => s[0] == "Line2");
+
+            Console.WriteLine(line2);
+        }
+
+        public static IReadOnlyList<string[]> ParseMessage(in string message)
+        {
+            var lines = message.Split(new[] { "\r" }, StringSplitOptions.RemoveEmptyEntries);
+            return lines.Select(ParseLine).ToList();
+        }
+
+        private static string[] ParseLine(in string segmentLine)
+        {
+            return segmentLine.Split(new[] { '|' }, StringSplitOptions.None);
+        }
+    }
+}
